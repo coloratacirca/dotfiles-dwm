81ac32a40b83081529f4836dd33e1e9e8eeb561d
coloratacirca <colorishm@gmail.com>
2023-09-12 17:20:52 +0200

    im so intelligent omg

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo mv $HOME/dotfiles-dwm/pacman.conf /etc/pacman.conf
 sudo pacman -Syy
-sudo pacman -Syu base base-devel xorg xorg-xinit libx11 fontconfig libxinerama libxft freetype2 alacritty polybar rofi picom alsa-utils pipewire-alsa pipewire-jack pipewire-pulse wireplumber noto-fonts noto-fonts-extra noto-fonts-emoji ttf-font-awesome ttf-iosevka unzip firefox
+sudo pacman -Syu base base-devel xorg xorg-xinit libx11 fontconfig libxinerama libxft freetype2 alacritty polybar rofi picom alsa-utils pipewire-alsa pipewire-jack pipewire-pulse wireplumber noto-fonts noto-fonts-extra noto-fonts-emoji ttf-font-awesome ttf-iosevka unzip firefox feh
 unzip Geologica.zip
 sudo mkdir -p /usr/local/share/fonts
 sudo mv static/* /usr/local/share/fonts
